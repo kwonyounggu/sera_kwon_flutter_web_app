20c94d13a51a5a23872479a2514afcb037225cde
Misoon Lim <misoonlim@Misoons-MacBook-Pro.local>
2025-03-03 21:42:27 -0500

    mar 3

--- lib/pages/login/login_screen_ds.dart
+++ lib/pages/login/login_screen_ds.dart
@@ -11,37 +11,48 @@ class LoginScreen extends StatefulWidget
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen> 
+{
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   bool _obscurePassword = true;
   bool _isChecked = false;
 
-  Future<void> _login() async {
+  Future<void> _login() async 
+  {
     if (_formKey.currentState!.validate()) 
     {
       // Add login logic here
       print('Email: ${_emailController.text}');
       print('Password: ${_passwordController.text}');
-      final response = await http.post(
+      final response = await http.post
+      (
         Uri.parse('http://your-ec2-ip:8000/login'),
         headers: {'Content-Type': 'application/json'},
-        body: json.encode({
+        body: json.encode
+        (
+          {
           'username': _emailController.text,
           'password': _passwordController.text,
-        }),
+          }
+        ),
       );
 
-      if (response.statusCode == 200) {
+      if (response.statusCode == 200) 
+      {
         final token = json.decode(response.body)['access_token'];
         // Save the token to localStorage or secure storage
         print('Logged in with token: $token');
-        ScaffoldMessenger.of(context).showSnackBar(
+        ScaffoldMessenger.of(context).showSnackBar
+        (
           SnackBar(content: Text('Login successful!')),
         );
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
+      } 
+      else 
+      {
+        ScaffoldMessenger.of(context).showSnackBar
+        (
           SnackBar(content: Text('Login failed: ${response.body}')),
         );
       }
@@ -49,35 +60,48 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+  Widget build(BuildContext context) 
+  {
+    return Scaffold
+    (
+      appBar: AppBar
+      (
         title: Text('Login'),
       ),
-      body: Padding(
+      body: Padding
+      (
         padding: const EdgeInsets.all(16.0),
-        child: Form(
+        child: Form
+        (
           key: _formKey,
-          child: Column(
-            children: [
+          child: Column
+          (
+            children: 
+            [
               // Company Logo
-              Text(
+              Text
+              (
                 'Eye Doctor S KWON',
-                style: TextStyle(
+                style: TextStyle
+                (
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               SizedBox(height: 20),
 
-              TextFormField(
+              TextFormField
+              (
                 controller: _emailController,
-                decoration: InputDecoration(
+                decoration: InputDecoration
+                (
                   labelText: 'Email',
                   border: OutlineInputBorder(),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
+                validator: (value) 
+                {
+                  if (value == null || value.isEmpty) 
+                  {
                     return 'Please enter your email';
                   }
                   if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) 
@@ -88,26 +112,37 @@ class _LoginScreenState extends State<LoginScreen> {
                 },
               ),
               SizedBox(height: 16),
-              TextFormField(
+              TextFormField
+              (
                 controller: _passwordController,
-                decoration: InputDecoration(
+                decoration: InputDecoration
+                (
                   labelText: 'Password',
                   border: OutlineInputBorder(),
-                  suffixIcon: IconButton(
+                  suffixIcon: IconButton
+                  (
                     icon: Icon(_obscurePassword ? Icons.visibility : Icons.visibility_off),
-                    onPressed: () {
-                      setState(() {
-                        _obscurePassword = !_obscurePassword;
-                      });
+                    onPressed: () 
+                    {
+                      setState
+                      (
+                        () 
+                        {
+                          _obscurePassword = !_obscurePassword;
+                        }
+                      );
                     },
                   ),
                 ),
                 obscureText: _obscurePassword,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
+                validator: (value) 
+                {
+                  if (value == null || value.isEmpty) 
+                  {
                     return 'Please enter your password';
                   }
-                  if (value.length < 6) {
+                  if (value.length < 6) 
+                  {
                     return 'Password must be at least 6 characters';
                   }
                   return null;
@@ -115,29 +150,40 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               SizedBox(height: 16),
               // Consent Radio Button
-                Row(
-                  children: [
-                    Radio(
+                Row
+                (
+                  children: 
+                  [
+                    Radio
+                    (
                       value: _isChecked,
                       groupValue: true,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          _isChecked = value ?? false;
-                        });
+                      onChanged: (bool? value) 
+                      {
+                        setState
+                        (
+                          () 
+                          {
+                            _isChecked = value ?? false;
+                          }
+                        );
                       },
                     ),
-                    Flexible(
-      child: Text(
-        'I confirm that I have read, consent and agree to your website\'s Terms of use and privacy',
-        style: TextStyle(fontSize: 14),
-        softWrap: true,
-      ),
-    ),
+                    Flexible
+                    (
+                      child: Text
+                      (
+                        'I confirm that I have read, consent and agree to your website\'s Terms of use and privacy',
+                        style: TextStyle(fontSize: 14),
+                        softWrap: true,
+                      ),
+                    ),
                   ],
                 ),
                 SizedBox(height: 20),
 
-              ElevatedButton(
+              ElevatedButton
+              (
                 onPressed: _login,
                 child: Text('Login'),
               ),
@@ -145,17 +191,22 @@ class _LoginScreenState extends State<LoginScreen> {
               SizedBox(height: 20),
 
                 // Forgot Password and Sign Up
-                Row(
+                Row
+                (
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextButton(
+                  children: 
+                  [
+                    TextButton
+                    (
                       onPressed: () {
                         // Add forgot password logic here
                       },
                       child: Text('Forgot Password?'),
                     ),
-                    TextButton(
-                      onPressed: () {
+                    TextButton
+                    (
+                      onPressed: () 
+                      {
                         // Add sign up logic here
                       },
                       child: Text('Sign Up'),
@@ -164,38 +215,50 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
                 SizedBox(height: 20),
               // Continue with Google
-                Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
+                Row
+                (
+                  children: 
+                  [
+                    Expanded
+                    (
+                      child: Divider
+                      (
                         color: Colors.grey,
                       ),
                     ),
-                    Padding(
+                    Padding
+                    (
                       padding: const EdgeInsets.symmetric(horizontal: 8.0),
                       child: Text('OR'),
                     ),
-                    Expanded(
-                      child: Divider(
+                    Expanded
+                    (
+                      child: Divider
+                      (
                         color: Colors.grey,
                       ),
                     ),
                   ],
                 ),
                 SizedBox(height: 20),
-                SizedBox(
+                SizedBox
+                (
                   width: double.infinity,
-                  child: ElevatedButton.icon(
-                    icon: Image.asset(
-                      'images/google_logo.jpg', // Add your Google logo asset here
+                  child: ElevatedButton.icon
+                  (
+                    icon: Image.asset
+                    (
+                      'images/google_logo.png', // Add your Google logo asset here
                       height: 24,
                     ),
                     label: Text('Continue with Google'),
-                    style: ElevatedButton.styleFrom(
+                    style: ElevatedButton.styleFrom
+                    (
                       foregroundColor: Colors.black, 
                       backgroundColor: Colors.white,
                     ),
-                    onPressed: () {
+                    onPressed: () 
+                    {
                       // Add Google login logic here
                     },
                   ),
