0baf5780177f10363b51bcacb7ad772894da05f7
Misoon Lim <misoonlim@Misoons-MacBook-Pro.local>
2025-03-13 22:51:00 -0400

    blog list and details

--- lib/pages/blog/blog_details.dart
+++ lib/pages/blog/blog_details.dart
@@ -4,8 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class BlogDetailPage extends ConsumerStatefulWidget 
-{
+class BlogDetailPage extends ConsumerStatefulWidget {
   final int blogId;
 
   const BlogDetailPage({super.key, required this.blogId});
@@ -15,42 +14,203 @@ class BlogDetailPage extends ConsumerStatefulWidget
   _BlogDetailPageState createState() => _BlogDetailPageState();
 }
 
-class _BlogDetailPageState extends ConsumerState<BlogDetailPage> 
-{
+class _BlogDetailPageState extends ConsumerState<BlogDetailPage> {
   dynamic _blog;
   List<dynamic> _comments = [];
+  bool _isLoading = true;
+  bool _isLoadingMoreComments = false;
+  bool _hasMoreComments = true; // Track if there are more comments to load
+  String _errorMessage = '';
+  int _commentPage = 1;
+  final int _commentsPerPage = 10; // Number of comments to load per page
+  final ScrollController _commentScrollController = ScrollController();
+  bool _showBackToTopButton = false; // Track if the "Back to Top" button should be shown
 
   @override
   void initState() {
     super.initState();
     fetchBlog();
     fetchComments();
+    _commentScrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _commentScrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    // Show/hide the "Back to Top" button based on scroll position
+    if (_commentScrollController.offset >= 400 && !_showBackToTopButton) {
+      setState(() {
+        _showBackToTopButton = true;
+      });
+    } else if (_commentScrollController.offset < 400 && _showBackToTopButton) {
+      setState(() {
+        _showBackToTopButton = false;
+      });
+    }
+
+    // Load more comments when the user scrolls to the bottom
+    if (_commentScrollController.position.pixels == _commentScrollController.position.maxScrollExtent && !_isLoadingMoreComments && _hasMoreComments) {
+      setState(() {
+        _commentPage++;
+      });
+      fetchComments(loadMore: true);
+    }
   }
 
   Future<void> fetchBlog() async {
-    final response = await http.get(Uri.parse('$FASTAPI_URL/blogs/${widget.blogId}')); // Replace with your FastAPI endpoint
+    try {
+      final response = await http.get(Uri.parse('$FASTAPI_URL/blogs/${widget.blogId}'));
 
-    if (response.statusCode == 200) {
+      if (response.statusCode == 200) {
+        setState(() {
+          _blog = json.decode(response.body);
+        });
+      } else {
+        throw Exception('Failed to load blog');
+      }
+    } catch (e) {
       setState(() {
-        _blog = json.decode(response.body);
+        _errorMessage = 'Failed to load blog: $e';
       });
-    } else {
-      throw Exception('Failed to load blog');
     }
   }
 
-  Future<void> fetchComments() async {
-      final response = await http.get(Uri.parse('$FASTAPI_URL/blogs/${widget.blogId}/comments')); // Replace with your FastAPI endpoint
-
-    if (response.statusCode == 200) {
+  Future<void> fetchComments({bool loadMore = false}) async {
+    try {
       setState(() {
-        _comments = json.decode(response.body);
+        if (loadMore) {
+          _isLoadingMoreComments = true;
+        } else {
+          _isLoading = true;
+        }
       });
-    } else {
-      throw Exception('Failed to load comments');
+
+      final response = await http.get(Uri.parse('$FASTAPI_URL/blogs/${widget.blogId}/comments?page=$_commentPage&per_page=$_commentsPerPage'));
+
+      if (response.statusCode == 200) {
+        final newComments = json.decode(response.body);
+        setState(() {
+          if (loadMore) {
+            _comments.addAll(newComments);
+          } else {
+            _comments = newComments;
+          }
+          _isLoading = false;
+          _isLoadingMoreComments = false;
+          _hasMoreComments = newComments.length >= _commentsPerPage; // Check if there are more comments to load
+        });
+      } else {
+        throw Exception('Failed to load comments');
+      }
+    } catch (e) {
+      setState(() {
+        _errorMessage = 'Failed to load comments: $e';
+        _isLoading = false;
+        _isLoadingMoreComments = false;
+      });
     }
   }
 
+  Future<void> _refreshComments() async {
+    setState(() {
+      _commentPage = 1;
+      _hasMoreComments = true; // Reset the "has more" flag when refreshing
+    });
+    await fetchComments();
+  }
+
+  void _scrollToTop() {
+    _commentScrollController.animateTo(
+      0,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  Widget _buildBlogContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Hero(
+          tag: 'blog-image-${_blog['blog_id']}',
+          child: Image.network(
+            _blog['image_url'] ?? 'https://picsum.photos/150',
+            height: 200,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          _blog['title'],
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        Text(
+          _blog['content'],
+          style: TextStyle(fontSize: 16),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildComments() {
+    return Expanded(
+      child: RefreshIndicator(
+        onRefresh: _refreshComments,
+        child: ListView.builder(
+          controller: _commentScrollController,
+          itemCount: _comments.length + (_isLoadingMoreComments ? 1 : 0) + (_hasMoreComments ? 0 : 1), // Add 1 for "No more items"
+          itemBuilder: (context, index) {
+            if (index == _comments.length) {
+              if (_isLoadingMoreComments) {
+                return Center(child: CircularProgressIndicator());
+              } else if (!_hasMoreComments) {
+                return Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Center(
+                    child: Text(
+                      'No more comments',
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                  ),
+                );
+              }
+            }
+            final comment = _comments[index];
+            return AnimatedOpacity(
+              opacity: 1.0,
+              duration: Duration(milliseconds: 500),
+              child: Card(
+                margin: EdgeInsets.symmetric(vertical: 8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        comment['content'],
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'By: ${comment['user']['name'] ?? 'Anonymous'}',
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,57 +218,54 @@ class _BlogDetailPageState extends ConsumerState<BlogDetailPage>
       appBar: AppBar(
         title: Text(_blog != null ? _blog['title'] : 'Loading...'),
       ),
-      body: _blog != null
-          ? Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    _blog['title'],
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    _blog['content'],
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Comments',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: _comments.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          margin: EdgeInsets.symmetric(vertical: 8.0),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  _comments[index]['content'],
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                                SizedBox(height: 5),
-                                Text(
-                                  'By: ${_comments[index]['user']['name'] ?? 'Anonymous'}',
-                                  style: TextStyle(fontSize: 12, color: Colors.grey),
-                                ),
-                              ],
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : _errorMessage.isNotEmpty
+              ? Center(child: Text(_errorMessage))
+              : Stack(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _buildBlogContent(),
+                        SizedBox(height: 20),
+                        Text(
+                          'Comments',
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        _buildComments(),
+                        if (_isLoadingMoreComments)
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: CircularProgressIndicator(),
+                          ),
+                        if (!_isLoadingMoreComments && _hasMoreComments)
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  _commentPage++;
+                                });
+                                fetchComments(loadMore: true);
+                              },
+                              child: Text('Load More Comments'),
                             ),
                           ),
-                        );
-                      },
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            )
-          : Center(child: CircularProgressIndicator()),
+                    if (_showBackToTopButton)
+                      Positioned(
+                        bottom: 20,
+                        right: 20,
+                        child: FloatingActionButton(
+                          onPressed: _scrollToTop,                      
+                          backgroundColor: Colors.blue,
+                          child: Icon(Icons.arrow_upward),
+                        ),
+                      ),
+                  ],
+                ),
     );
   }
 }
--- lib/pages/blog/public_blog_listing.dart
+++ lib/pages/blog/public_blog_listing.dart
@@ -17,35 +17,148 @@ class PublicBlogListing extends ConsumerStatefulWidget
 class _PublicBlogListingState extends ConsumerState<PublicBlogListing> 
 {
   List<dynamic> _blogs = [];
+  bool _isLoading = true;
+  bool _isLoadingMore = false;
+  bool _hasMore = true; // Track if there are more items to load
+  String _errorMessage = '';
+  int _page = 1;
+  final int _perPage = 10; // Number of blogs to load per page
+  final ScrollController _scrollController = ScrollController();
+  bool _showBackToTopButton = false; // Track if the "Back to Top" button should be shown
 
   @override
   void initState() 
   {
     super.initState();
     fetchBlogs();
+    _scrollController.addListener(_scrollListener);
   }
 
-  Future<void> fetchBlogs() async 
+  @override
+  void dispose() 
   {
-    final response = await http.get(Uri.parse('$FASTAPI_URL/blogs?visibility=DoctorsOnly')); // Replace with your FastAPI endpoint
+    _scrollController.dispose();
+    super.dispose();
+  }
 
-    if (response.statusCode == 200) 
+  void _scrollListener() 
+  {
+    // Show/hide the "Back to Top" button based on scroll position
+    if (_scrollController.offset >= 400 && !_showBackToTopButton) 
     {
-      //print(response.body);
-      print(json.decode(response.body));
       setState
       (
         () 
         {
-          //_blogs = json.decode(response.body);
-          
+          _showBackToTopButton = true;
         }
       );
     } 
-    else 
+    else if (_scrollController.offset < 400 && _showBackToTopButton) 
     {
-      throw Exception('Failed to load blogs');
+      setState
+      (
+        () 
+        {
+          _showBackToTopButton = false;
+        }
+      );
     }
+
+    // Load more data when the user scrolls to the bottom
+    if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent && !_isLoadingMore && _hasMore) 
+    {
+      setState
+      (
+        () 
+        {
+          _page++;
+        }
+      );
+      fetchBlogs(loadMore: true);
+    }
+  }
+
+  Future<void> fetchBlogs({bool loadMore = false}) async 
+  {
+    try 
+    {
+      setState
+      (
+        () 
+        {
+          if (loadMore) 
+          {
+            _isLoadingMore = true;
+          } 
+          else 
+          {
+            _isLoading = true;
+          }
+        }
+      );
+
+      final response = await http.get(Uri.parse('$FASTAPI_URL/blogs?visibility=Public&page=$_page&per_page=$_perPage'));
+
+      if (response.statusCode == 200) 
+      {
+        final newBlogs = json.decode(response.body);
+        setState
+        (
+          () 
+          {
+          if (loadMore) 
+          {
+            _blogs.addAll(newBlogs);
+          } 
+          else 
+          {
+            _blogs = newBlogs;
+          }
+          _isLoading = false;
+          _isLoadingMore = false;
+          _hasMore = newBlogs.length >= _perPage; // Check if there are more items to load
+        });
+      } 
+      else 
+      {
+        throw Exception('Failed to load blogs');
+      }
+    } 
+    catch (e) 
+    {
+      setState
+      (
+        () 
+        {
+          _errorMessage = 'Failed to load blogs: $e';
+          _isLoading = false;
+          _isLoadingMore = false;
+        });
+    }
+  }
+
+  Future<void> _refreshBlogs() async 
+  {
+    setState
+    (
+      () 
+      {
+        _page = 1;
+        _hasMore = true; // Reset the "has more" flag when refreshing
+      }
+    );
+    await fetchBlogs();
+  }
+
+  void _scrollToTop() 
+  {
+    _scrollController.animateTo
+    (
+      0,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
   }
 
   @override
@@ -57,31 +170,161 @@ class _PublicBlogListingState extends ConsumerState<PublicBlogListing>
       (
         title: Text('Public Blog'),
       ),
-      body: ListView.builder
+      body: RefreshIndicator
       (
-        itemCount: _blogs.length,
-        itemBuilder: (context, index) 
-        {
-          return Card
-          (
-            margin: EdgeInsets.all(8.0),
-            child: ListTile
-            (
-              title: Text(_blogs[index]['title']),
-              subtitle: Text(_blogs[index]['content'].substring(0, 100) + '...'), // Show a snippet
-              onTap: () 
-              {
-                Navigator.push
+        onRefresh: _refreshBlogs,
+        child: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : _errorMessage.isNotEmpty
+                ? Center(child: Text(_errorMessage))
+                : Stack
                 (
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => BlogDetailPage(blogId: _blogs[index]['blog_id']),
+                    children: 
+                    [
+                      Column
+                      (
+                        children: 
+                        [
+                          Expanded
+                          (
+                            child: ListView.builder
+                            (
+                              controller: _scrollController,
+                              itemCount: _blogs.length + (_isLoadingMore ? 1 : 0) + (_hasMore ? 0 : 1), // Add 1 for "No more items"
+                              itemBuilder: (context, index) 
+                              {
+                                if (index == _blogs.length) 
+                                {
+                                  if (_isLoadingMore) 
+                                  {
+                                    return Center(child: CircularProgressIndicator());
+                                  } 
+                                  else if (!_hasMore) 
+                                  {
+                                    return Padding
+                                    (
+                                      padding: EdgeInsets.all(16.0),
+                                      child: Center
+                                      (
+                                        child: Text
+                                        (
+                                          'No more items',
+                                          style: TextStyle(fontSize: 16, color: Colors.grey),
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                }
+                                final blog = _blogs[index];
+                                return AnimatedOpacity
+                                (
+                                  opacity: 1.0,
+                                  duration: Duration(milliseconds: 500),
+                                  child: Card
+                                  (
+                                    margin: EdgeInsets.all(8.0),
+                                    elevation: 4.0,
+                                    child: InkWell
+                                    (
+                                      onTap: () 
+                                      {
+                                        Navigator.push
+                                        (
+                                          context,
+                                          MaterialPageRoute
+                                          (
+                                            builder: (context) => BlogDetailPage(blogId: blog['blog_id']),
+                                          ),
+                                        );
+                                      },
+                                      child: Column
+                                      (
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: 
+                                        [
+                                          Hero
+                                          (
+                                            tag: 'blog-image-${blog['blog_id']}',
+                                            child: Image.network(
+                                              blog['image_url'] ?? 'https://picsum.photos/150',
+                                              height: 150,
+                                              width: double.infinity,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          Padding
+                                          (
+                                            padding: EdgeInsets.all(8.0),
+                                            child: Column
+                                            (
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: 
+                                              [
+                                                Text
+                                                (
+                                                  blog['title'],
+                                                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                                ),
+                                                SizedBox(height: 8),
+                                                Text(
+                                                  _blogs[index]['content'] != null && _blogs[index]['content'].length > 100
+                                                                                      ? _blogs[index]['content'].substring(0, 100) + '...'
+                                                                                      : _blogs[index]['content'] ?? '',
+                                                  style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                          if (_isLoadingMore)
+                            Padding
+                            (
+                              padding: EdgeInsets.all(8.0),
+                              child: CircularProgressIndicator(),
+                            ),
+                          if (!_isLoadingMore && _hasMore)
+                            Padding
+                            (
+                              padding: EdgeInsets.all(8.0),
+                              child: ElevatedButton
+                              (
+                                onPressed: () 
+                                {
+                                  setState
+                                  (
+                                    () 
+                                    {
+                                      _page++;
+                                    }
+                                  );
+                                  fetchBlogs(loadMore: true);
+                                },
+                                child: Text('Load More'),
+                              ),
+                            ),
+                        ],
+                      ),
+                      if (_showBackToTopButton)
+                        Positioned
+                        (
+                          bottom: 20,
+                          right: 20,
+                          child: FloatingActionButton
+                          (
+                            onPressed: _scrollToTop,
+                            backgroundColor: Colors.blue,
+                            child: Icon(Icons.arrow_upward),
+                          ),
+                        ),
+                    ],
                   ),
-                );
-              },
-            ),
-          );
-        },
       ),
     );
   }
